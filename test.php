cda0fa9d68fe9dbaea0f9ffd3695198f66866f02
Luis Portillo Miranda <miranda.luis@gmail.com>
2016-10-14 18:15:10 -0300

    Ejemplos de configuración
    Se agregó el ejemplo de configuración

--- test.php
+++ test.php
@@ -1,5 +1,12 @@
 <?php
 require 'bancard.class.php';
-$bancard = new Bancard();
+
+/*
+* Procesar transaccion
+*/
+$bancard = new Bancard(true);
+$bancard->set_response_mode("REDIRECT");
+$bancard->set_item_description("Producto color blanco");
 $response = $bancard->process(1356, 465000);
 print $response;
+
